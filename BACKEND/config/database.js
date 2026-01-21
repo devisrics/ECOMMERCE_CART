98cc9d257f08884298a8445f3b45a237e8661314
DEVISRI V E <devisrics13@gmail.com>
2026-01-22 00:07:51 +0530

    Update database.js

--- BACKEND/config/database.js
+++ BACKEND/config/database.js
@@ -1,7 +1,14 @@
+// database.js
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Load env immediately
+dotenv.config({ path: path.resolve(__dirname, 'config.env') });
+
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-  console.log('Connecting to MongoDB with URI:', process.env.DB_LOCAL_URI); // add this line
+  console.log('Connecting to MongoDB with URI:', process.env.DB_LOCAL_URI); // debug
   mongoose.connect(process.env.DB_LOCAL_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
