04466cee3ac32fe90f9b7256eaecb4f377301979
DEVISRI V E <devisrics13@gmail.com>
2026-01-22 16:36:01 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -7,7 +7,7 @@ const errorMiddleware = require('./middlewares/error');
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || true,
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
