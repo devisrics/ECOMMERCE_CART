8ab78c1c106c322cf20efad7cba5b1447739254f
DEVISRI V E <devisrics13@gmail.com>
2026-01-22 23:01:23 +0530

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,24 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import productsReducer from "./slices/productsSlice";
-import productReducer from './slices/productSlice';
-import authReducer from './slices/authSlice';
-import cartReducer from './slices/cartSlice';
-import orderReducer from './slices/orderSlice';
-import userReducer from './slices/userSlice'
 
+import productsReducer from "./slices/productsSlice";
+import productReducer from "./slices/productSlice";
+import authReducer from "./slices/authSlice";
+import cartReducer from "./slices/cartSlice";
+import orderReducer from "./slices/orderSlice";
+import userReducer from "./slices/userSlice";
 
 const reducer = combineReducers({
     productsState: productsReducer,
-    productState: productReducer ,
+    productState: productReducer,
     authState: authReducer,
     cartState: cartReducer,
     orderState: orderReducer,
-    userState: userReducer
-})
-
+    userState: userReducer,
+});
 
 const store = configureStore({
     reducer,
-    middleware: [thunk]
-})
+    devTools: process.env.NODE_ENV !== "production",
+});
 
 export default store;
