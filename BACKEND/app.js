c9560684ad7e433980472dc56790a61c6889deaa
DEVISRI V E <devisrics13@gmail.com>
2026-01-23 01:54:36 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -22,11 +22,18 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('/*', (req, res) => {
+  app.use(express.static(path.join(__dirname, '../frontend/build'), {
+    // Ensure proper handling of range requests
+    acceptRanges: true,
+    cacheControl: true,
+    etag: true
+  }));
+
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   });
 }
+
 
 app.use(errorMiddleware)
 
