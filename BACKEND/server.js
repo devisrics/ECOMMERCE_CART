cf55d0ebfb7492377e8106ce996b358f591d579f
DEVISRI V E <devisrics13@gmail.com>
2026-01-23 11:51:52 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,26 +4,23 @@ const connectDatabase = require('./config/database');
 
 connectDatabase();
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
-process.on('unhandledRejection',(err)=>{
+process.on('unhandledRejection', (err) => {
     console.log(`Error: ${err.message}`);
     console.log('Shutting down the server due to unhandled rejection error');
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
-    })
-})
+    });
+});
 
-process.on('uncaughtException',(err)=>{
+process.on('uncaughtException', (err) => {
     console.log(`Error: ${err.message}`);
     console.log('Shutting down the server due to uncaught exception error');
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
-    })
-})
-
+    });
+});
 
